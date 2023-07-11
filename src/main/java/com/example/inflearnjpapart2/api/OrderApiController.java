1b18578438c47791592dfb6f8dfd821431482c3e
jang-yujin <jyj9747@naver.com>
2023-07-11 15:30:34 +0900

    :sparkles: Section4-1 : 엔티티 직접 노출
    .

---
+++ src/main/java/com/example/inflearnjpapart2/api/OrderApiController.java
@@ -0,0 +1,31 @@
+package com.example.inflearnjpapart2.api;
+
+import com.example.inflearnjpapart2.domain.Order;
+import com.example.inflearnjpapart2.domain.OrderItem;
+import com.example.inflearnjpapart2.domain.OrderSearch;
+import com.example.inflearnjpapart2.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
+}
